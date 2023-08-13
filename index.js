8f3a718e9282c051919b83ba0af6bf9e8355023e
Benjamin Dordoigne <benjamindordoigne@benjamins-air.home>
2023-08-13 09:45:50 +0100

    updated express

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const express = require('express');
+const cheerio = require('cheerio');
+
+const PORT = 8000;
+const app = express();
+
+app.listen(PORT, () => console.log(`Port num is ${PORT}`));
+
+// GET
+app.get("/", (req, res) => {
+    //res.json('Hello');
+    res.send({
+        "data": "benajjjs"
+    })
+    console.log('Hit home page');
+});
+
+
+app.get("/news", (req, res) => {
+ 
+    console.log('Hit news page');
+});
+
+
